b4971b8abaa46c8b08c0c7bff0671bdd244bfbad
H4rsh4nk <pathakharsh13@gmail.com>
2023-07-12 17:41:44 -0400

    modified:   package-lock.json 	modified:   package.json 	new file:   pages/index.js 	deleted:    pages/index.tsx 	modified:   pages/zoom.js 	modified:   styles/globals.css 	modified:   styles/zoom.module.css 	modified:   tsconfig.json

--- pages/zoom.js
+++ pages/zoom.js
@@ -1,10 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import router from 'next/router';
-import MeetingStyles from '../styles/zoom.module.css';
 const axios = require('axios');
 const { ZOOM } = require('../Constants/Zoom')
 
 const Meeting = () => {
+    
+    //Chatbox component
+    const [message, setMessage] = useState('');
+    const [displayedMessage, setDisplayedMessage] = useState('');
+    const [isMessageVisible, setIsMessageVisible] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setDisplayedMessage(message);
+        setMessage('');
+        setIsMessageVisible(true);
+    };
+
+    const handleChange = (e) => {
+        setMessage(e.target.value);
+    };
+
+    const handleCloseMessage = () => {
+        setIsMessageVisible(false);
+    };
+
+    //Zoom SDK
     useEffect(() => {
         return async () => {
             new Promise(async (resolve, reject) => {
@@ -21,8 +42,8 @@ const Meeting = () => {
                         video: {
                             viewSizes: {
                                 default: {
-                                    height: 600,
-                                    width: 600
+                                    height: 500,
+                                    width: 700
                                 },
                                 ribbon: {
                                     width: 400
@@ -37,8 +58,9 @@ const Meeting = () => {
                     }
                 });
 
-                let payload = router.query;
+                
 
+                let payload = router.query;
                 const { data } = await axios({
                     url: '/api/Zoom/',
                     method: 'POST',
@@ -58,42 +80,58 @@ const Meeting = () => {
                     userEmail: '',
                     tk: '',
                     zak: ''
-                 })
+                })
+
             }).catch(error => {
                 console.log('---Error inside useEffect---', error);
             });
+
+            
         }
     });
+
     return (
-        <div className={MeetingStyles.container}>
-            <div className={MeetingStyles.meetingSDKElement} id='meetingSDKElement'></div>
-            <div className={MeetingStyles.content}>
-               
-                <form>
-                    <div className='flex'>
-                        <div className='flex-col'>
-                            <label htmlFor='meetingID' className='label flex'>Meeting ID:</label>
-                            <input type='text' id='meetingID' placeholder='Enter Meeting ID' />
-                        </div>
-                        <div className='flex-col'>
-                            <label htmlFor='password' className='label flex'>Password:</label>
-                            <input type='password' id='password' placeholder='Enter Password' />
-                        </div>
-                        <div className='flex-col'>
-                            <label htmlFor='userName' className='label flex'>User Name:</label>
-                            <input type='text' id='userName' placeholder='Enter User Name' />
+        <div>
+            {isMessageVisible && (
+                <div className="flex justify-center" id="messageContainer">
+                    <div className="bg-green-500 text-white p-2 rounded-lg mb-2">
+                        <div className="flex items-center">
+                            <span className="mr-2">{displayedMessage}</span>
+                            
+                            <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6" onClick={handleCloseMessage}>
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                            </button>
                         </div>
                     </div>
-                    <button className='flex border-2' type="submit" value="Submit" onClick=''> Join </button>
-                </form>
-                
+                </div>
+            )}
+            <div className="flex">
+                <div className="w-70 flex-auto" id="meetingSDKElement"></div>
+                <div className="w-30 flex justify-end items-end">
+                    <form className="mt-2 flex" onSubmit={handleSubmit}>
+                        <input
+                            className="rounded-l-lg textbox"
+                            type="text"
+                            value={message}
+                            onChange={handleChange}
+                            placeholder="Type your message..."
+                        />
+                        <button
+                            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg"
+                            type="submit"
+                        >
+                            Send
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div>)
+        </div>
 
-
+    )
 }
 
 Meeting.displayName = 'Zoom Component View';
 
 export default Meeting;
-
